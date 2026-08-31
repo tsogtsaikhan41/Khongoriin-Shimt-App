d0534c28d2fa079b429f5cc64e82fe61dfc85141
tsogtsaikhan41 <tsogtsaikhan41@gmail.com>
2026-08-31 12:50:34 +0800

    Update config.js
    UPdated

--- khongorshimt/public/config.js
+++ khongorshimt/public/config.js
@@ -9,4 +9,4 @@ window.SUPABASE_ANON_KEY = "sb_publishable_LwOtNMXJiVZr2Z2EXBPhRw_6ndrzjqm";
 
 // A fake email domain used internally so staff can log in with just
 // a username instead of a real email address. Not shown to users.
-window.AUTH_EMAIL_DOMAIN = "khongorshimt.internal";
+window.AUTH_EMAIL_DOMAIN = "khongorshimt-staff.com";
